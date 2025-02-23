2c37d12bf379829239bcb3089bf50d8cb4c1b382
Abdelrahman Ahmed <supersanko2002@gmail.com>
2025-02-23 23:34:29 +0200

    Started creating an options box for the case, such as the color, design material, model, and so on.

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,12 +1,25 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
 
 import { cn } from "@/lib/utils";
 import { Rnd } from "react-rnd";
+import { RadioGroup } from "@headlessui/react";
 
-import { AspectRatio } from "@/app/_components/ui/aspect-ratio";
 import HandleComponent from "@/app/_components/HandleComponent";
+import { AspectRatio } from "@/app/_components/ui/aspect-ratio";
+import { ScrollArea } from "@/app/_components/ui/scroll-area";
+import { Label } from "@/app/_components/ui/label";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/app/_components/ui/dropdown-menu";
+import { Button } from "@/app/_components/ui/button";
+import { COLORS, MODELS } from "@/app/_validators/option-validator";
+import { Check, ChevronsUpDown } from "lucide-react";
 
 interface DesignConfiguratorProps {
   configId: string;
@@ -19,6 +32,14 @@ export default function DesignConfigurator({
   imageUrl,
   imageDimensions,
 }: DesignConfiguratorProps) {
+  const [options, setOptions] = useState<{
+    color: (typeof COLORS)[number];
+    model: (typeof MODELS.options)[number];
+  }>({
+    color: COLORS[0],
+    model: MODELS.options[0],
+  });
+
   return (
     <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
       <div
@@ -47,7 +68,7 @@ export default function DesignConfigurator({
           <div
             className={cn(
               "absolute inset-0 left-[3px] right-[3px] top-px bottom-px rounded-[32px]",
-              `bg-zinc-950`
+              `bg-${options.color.tw}`
             )}
           />
         </div>
@@ -77,6 +98,105 @@ export default function DesignConfigurator({
             />
           </div>
         </Rnd>
+      </div>
+
+      <div className="h-[37.5rem] flex flex-col bg-white">
+        <ScrollArea className="relative flex-1 overflow-auto">
+          <div
+            className="absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t
+            from-white pointer-events-none"
+          />
+
+          <div className="px-8 pb-12 pt-8">
+            <h2 className="tracking-tight font-bold text-3xl">
+              Customize your case
+            </h2>
+
+            <div className="w-full h-px bg-zinc-200 my-6" />
+
+            <div className="relative mt-4 h-full flex flex-col justify-between">
+              <div className="flex flex-col gap-6">
+                <RadioGroup
+                  value={options.color}
+                  onChange={(val) => {
+                    setOptions((prev) => ({
+                      ...prev,
+                      color: val,
+                    }));
+                  }}
+                >
+                  <Label>Color: {options.color.label}</Label>
+                  <div className="mt-3 flex items-center space-x-3">
+                    {COLORS.map((color) => (
+                      <RadioGroup.Option
+                        key={color.label}
+                        value={color}
+                        className={({ active, checked }) =>
+                          cn(
+                            "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent",
+                            {
+                              [`border-${color.tw}`]: active || checked,
+                            }
+                          )
+                        }
+                      >
+                        <span
+                          className={cn(
+                            `bg-${color.tw}`,
+                            "size-8 rounded-full border border-black border-opacity-10"
+                          )}
+                        />
+                      </RadioGroup.Option>
+                    ))}
+                  </div>
+                </RadioGroup>
+
+                <div className="relative flex flex-col gap-3 w-full">
+                  <Label>Model</Label>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button
+                        role="combobox"
+                        variant="outline"
+                        className="w-full justify-between"
+                      >
+                        {options.model.label}
+                        <ChevronsUpDown className="ml-2 size-4 shrink-0 opacity-50" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      {MODELS.options.map((model) => (
+                        <DropdownMenuItem
+                          key={model.label}
+                          className={cn(
+                            "flex text-sm gap-1 items-center p-1.5 cursor-default hover:bg-zinc-100",
+                            {
+                              "bg-zinc-100":
+                                model.label === options.model.label,
+                            }
+                          )}
+                          onClick={() => {
+                            setOptions((prev) => ({ ...prev, model }));
+                          }}
+                        >
+                          <Check
+                            className={cn(
+                              "mr-2 size-4",
+                              model.label === options.model.label
+                                ? "opacity-100"
+                                : "opacity-0"
+                            )}
+                          />
+                          {model.label}
+                        </DropdownMenuItem>
+                      ))}
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
+              </div>
+            </div>
+          </div>
+        </ScrollArea>
       </div>
     </div>
   );
